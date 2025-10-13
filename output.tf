e6eef3152f5c0d5d9b19cf2e8a1b088deadb8277
RajeshMukkudipalli <56247918+RajeshMukkudipalli@users.noreply.github.com>
2025-10-13 21:14:38 +0530

    Update output.tf
    we are changing few lines

--- output.tf
+++ output.tf
@@ -2,7 +2,8 @@ output "sg_id" {
     value = aws_security_group.main.id
 }
 
-output "public_subnet_ids" {
-    value = aws_subnet.public[*].id
+#output "public_subnet_ids" {
+    #value = aws_subnet.public[*].id
 
-}
+#}
+
